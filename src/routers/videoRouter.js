6bf25ba116f178e740f3467f390050363d6cc84c
dongmin7208 <sayg3o3d@gmail.com>
2022-09-05 18:09:26 +0900

    getEdit + post  [6.2]

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express"
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import { see, getEdit, postEdit, deleteVideo, upload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 
-videoRouter.get("/upload", upload)
 videoRouter.get("/:id", see)
-videoRouter.get("/:id/edit", edit)
-videoRouter.get("/:id/delete", deleteVideo)
+videoRouter.get("/:id/edit", getEdit)
+videoRouter.post("/:id/edit", postEdit)
+// videoRouter.get("/:id/delete", deleteVideo)
+// videoRouter.get("/upload", upload)
 
 export default videoRouter
--- src/server.js
+++ src/server.js
@@ -1,82 +1,22 @@
-import express from "express"
+import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter"
-import videoRouter from "./routers/videoRouter"
-import userRouter from "./routers/userRouter"
-//app 이 아니라도괜찮음.
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
 const PORT = 4000;
-console.log(process.cwd());
+
 const app = express();
-const logger = morgan("dev")
+const logger = morgan("dev");
 
-app.set("view engine", "pug")
-app.set("views", process.cwd() + "/src/views")
-
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
-/*
-morgan("dev")
-GET / 304 3.444 ms - -
-GET /login 200 0.631 ms - 5
-*/
-
-
-
-
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-
-const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT} 🚀`)
+const handleListening = () =>
+    console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
-// const home = (req, res) => {
-//     console.log("I will respond.")
-//     return res.send("hello")
-// }
-// const login = (req, res) => {
-//     return res.send("login")
-// }
-// logger함수는 middleware를 return해준다.
-// app.get("/", home);
-// app.get("/login", login);
-
-
-// const logger = (req, res, next) => {
-//     console.log("I'm in the middle!");
-//     // return res.send("I havethe Power")
-//     console.log(`${req.method} ${req.url}`)
-//     next()
-// }
-// const privateMiddleware = (req, res, next) => {
-//     const url = req.url;
-//     if (url === '/protected') {
-//         return res.send("<h1>Not allowed</h1>")
-//     }
-//     console.log("Allowed, you may continue.")
-//     next();
-// }
-//requests, response
-// const handleHome = (req, res) => {
-//     // return res.end();
-//     // return res.send("<h1>hello!</h1>")
-//     return res.send("I love middlewares")
-// }
-//requests, response
-// const handleLogin = (req, res) => {
-//     // return res.end();
-//     // return res.send("Login here!")
-//     return res.send({ message: "Login here" })
-// }
-
-// const handleProtected = (req, res) => {
-//     return res.send("welcome to the private lounge.")
-// }
-// app.use(logger)
-// app.use(privateMiddleware);
-// app.get("/", handleHome);
-// app.get("/protected", handleProtected);
-// global middleware을 만들어줄수있음. 어느 url에도 작동시켜줌!
-
-// app.get("/login", handleLogin);
