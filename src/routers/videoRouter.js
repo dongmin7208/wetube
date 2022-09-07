c5550746d721a314cad8f86b9f2ee8344e9ad4eb
dongmin7208 <sayg3o3d@gmail.com>
2022-09-08 00:32:43 +0900

    videoRouter setting[6.5]

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,15 @@
 import express from "express"
-import { watch, getEdit, postEdit, deleteVideo, upload } from "../controllers/videoController";
+import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload)
+
+// videoRouter.get("/upload", getUpload)
+// videoRouter.post("/upload", postUpload)
 // videoRouter.get("/:id/edit", getEdit)
 // videoRouter.post("/:id/edit", postEdit)
 
